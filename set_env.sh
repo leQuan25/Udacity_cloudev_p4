2bac4efceea0e64bb3f775411377a383c1487841
leQuan25 <44423324+leQuan25@users.noreply.github.com>
2024-10-27 20:21:10 +0700

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=dbAdminp
+export POSTGRES_HOST=database-clouddev-project4.cahpbt8wxpm1.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:arn:aws:s3:::clouddev-project4-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
